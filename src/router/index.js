770e974649220669acb94ab801203353794f5ac9
zhaokai <46128592@qq.com>
2020-08-03 18:51:20 +0800

    修改用户信息

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/Welcome',
+      children: [{
+        path: '/welcome', component: Welcome
+      }, {
+        path: '/users', component: Users
+      }
+      ]
+    }
+  ]
+})
+// 挂在路由导航守卫
+router.beforeEach((to, nfrom, next) => {
+  // to将要访问的路径
+  // from代表从哪个路径跳转而来
+  // next是一个函数，表示放行
+  // next() 放行  next('/login)  强制跳转
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+export default router
