314ad8f96c3824bd47fd398829c2a1f2bd69019f
phoenixzk <461028592@qq.com>
2020-08-08 03:05:21 +0800

    权限添加角色完善

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
@@ -20,7 +21,9 @@ const router = new VueRouter({
       }, {
         path: '/users',
         component: Users
-      }, { path: '/rights', component: Rights }]
+      }, { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles }
+      ]
     }
   ]
 })
